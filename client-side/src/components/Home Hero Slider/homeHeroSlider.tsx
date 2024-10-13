bc83f5d5359710b169f308733f1c76fb3a4569f4
Khalida <xalide.haqverdiyeva@gmail.com>
2024-10-13 14:08:53 +0400

    fix: slider text animation bug fix

--- client-side/src/components/Home Hero Slider/homeHeroSlider.tsx
+++ client-side/src/components/Home Hero Slider/homeHeroSlider.tsx
@@ -1,7 +1,7 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 import React, { useState, useEffect } from "react";
 import { LuChevronLeft, LuChevronRight } from "react-icons/lu";
+import { motion } from "framer-motion";
 
 const HomeHeroSlider = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
@@ -11,16 +11,18 @@ const HomeHeroSlider = () => {
         "https://impact-theme-home.myshopify.com/cdn/shop/files/Mags_Corner_comb_2_Right_Lint_grey_Rebar_Square_Coffee_Table_marble_tabletop_soft_black_frame_Moire_Kelim_green_DLM_sun_yellow_cb7f484e-59a1-46ee-8c0b-8aa9b9971738.jpg?v=1657547898&width=2800",
       mobileSrc:
         "https://impact-theme-home.myshopify.com/cdn/shop/files/Mags_Corner_comb_2_Right_Lint_grey_Rebar_Square_Coffee_Table_marble_tabletop_soft_black_frame_Moire_Kelim_green_DLM_sun_yellow_mobile.jpg?v=1656590750&width=800",
-      text: "Hay PC Portable Lamp",
-      title: ["Pierre", "Charpin's", "refined lamp"],
+      text: "Decoration ",
+      title: ["Refresh your", "living room"],
+      button: "Shop Decoration",
     },
     {
       srcSet:
         "https://impact-theme-home.myshopify.com/cdn/shop/files/PC_Portable_olive_Palissade_Lounge_chair_olive_Palissade_Table_olive_Coffee_M_mint_Rainbow_mug.jpg?v=1656504940&width=2800",
       mobileSrc:
         "https://impact-theme-home.myshopify.com/cdn/shop/files/PC_Portable_olive_Palissade_Lounge_chair_olive_Palissade_Table_olive_Coffee_M_mint_Rainbow_mug_mobile.jpg?v=1656504947&width=800",
-      text: "Decoration",
-      title: ["Refresh your", "living room"],
+      text: "Hay PC Portable Lamp",
+      title: ["Pierre", "Charpin's", "refined lamp"],
+      button: "Discover",
     },
   ];
 
@@ -59,27 +61,32 @@ const HomeHeroSlider = () => {
                 className="w-full h-full object-cover"
               />
             </picture>
-            <div className="absolute left-4 bottom-16 z-20">
-              <div className="text-white text-[14px] font-bold">
-                {slide.text}
-              </div>
-              <div className="flex flex-col mt-2 z-50 overflow-hidden">
-                {slide.title.map((line, lineIndex) => (
-                  <div
-                    key={lineIndex}
-                    className={`transform text-[48px] text-white font-[700] transition-transform duration-700 ease-in-out`}
-                    style={{
+            <div className="absolute left-0 top-1/2 transform -translate-y-1/2 z-20 flex flex-col items-start p-[20px]">
+              <p className="text-white text-[16px] font-bold">{slide.text}</p>
+              <div className="flex flex-col mt-2 z-50 text-[50px] font-[900] leading-[1.1] text-white">
+                {slide.title.map((line, i) => (
+                  <motion.span
+                    key={`${currentSlide}-${i}`}
+                    initial={{ opacity: 0, y: 20, rotate: 4 }}
+                    animate={{
                       opacity: isCurrent ? 1 : 0,
-                      transform: isCurrent
-                        ? "rotate(0deg)" // Final position
-                        : "rotate(-10deg)", // Initial rotated position
-                      transitionDelay: `${lineIndex * 200}ms`, // Stagger the animations
+                      y: isCurrent ? 0 : 20,
+                      rotate: isCurrent ? 0 : 4,
                     }}
+                    transition={{
+                      delay: isCurrent ? i * 0.2 : 0,
+                      duration: 0.4,
+                      ease: "easeInOut",
+                    }}
+                    style={{ transformOrigin: "top left" }}
                   >
-                    {line} {/* Full line of text */}
-                  </div>
+                    {line}
+                  </motion.span>
                 ))}
               </div>
+              <button className="text-[#272727] cursor-pointer text-[14px] font-[800] mt-[24px] bg-white py-[16px] px-[32px]">
+                {slide.button}
+              </button>
             </div>
           </div>
         );
