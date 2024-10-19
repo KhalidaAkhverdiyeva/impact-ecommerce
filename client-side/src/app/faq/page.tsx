bb08608f1da977c4d343fb65d77978bbae597af0
Khalida <xalide.haqverdiyeva@gmail.com>
2024-10-19 22:04:35 +0400

    feat: accoridon added for faq page

--- client-side/src/app/faq/page.tsx
+++ client-side/src/app/faq/page.tsx
@@ -1,3 +1,4 @@
+import Accordion from "@/components/FAQ Accardion/accardion";
 import { HeaderWhite } from "@/components/Layout/Header/header";
 import MessageForm from "@/components/Message Form/messageForm";
 import ShopifySection from "@/components/Shopify Section/shopifySection";
@@ -7,6 +8,41 @@ const FAQPage = () => {
   return (
     <div>
       <HeaderWhite />
+      <div className=" h-[560px] pt-[66px] relative overflow-hidden">
+        <img
+          src="https://impact-theme-home.myshopify.com/cdn/shop/files/faq.jpg?v=1658917308&width=3200"
+          alt="contact"
+          className=" object-cover absolute inset-0 w-full h-full"
+        />
+
+        <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-25 ">
+          <h2 className="text-white font-[800] text-[40px] md:text-[48px] lg:text-[68px] leading-[1]  text-center max-w-[760px]">
+            Frequently Asked Questions
+          </h2>
+        </div>
+      </div>
+      <div className="mx-auto max-w-[1600px]">
+        <div className="px-[20px] py-[48px] md:px-[32px] lg:px-[48px]">
+          <h2 className="text-[32px] text-[#272727] font-[800] text-center">
+            Popular questions from our customers
+          </h2>
+          <div className="max-w-xl mx-auto mt-10 bg-[#F4F4F4]">
+            <Accordion
+              title="How do you calculate your shipping and handling charges?"
+              content="We’re dedicated to offering the best shipping methods available to ensure that your order reaches you quickly and safely. Shipping rates will vary, based on the specified delivery method, shipping address and total price of the merchandise in your cart."
+            />
+            <Accordion
+              title="Accordion Item 2"
+              content="This is the content of the second accordion item."
+            />
+            <Accordion
+              title="Accordion Item 3"
+              content="This is the content of the third accordion item."
+            />
+          </div>
+        </div>
+      </div>
+
       <div className="bg-[#EFEFEF]">
         <div className=" max-w-[900px] mx-auto  flex flex-col px-[20px] md:px-[32px] lg:px-[48px] py-[50px] lg:py-[78px]">
           <div className="flex flex-col gap-[20px] text-[#272727] pb-[48px]">
--- client-side/src/components/FAQ Accardion/accardion.tsx
+++ client-side/src/components/FAQ Accardion/accardion.tsx
@@ -1,7 +1,62 @@
-import React from "react";
+"use client";
+import { useState } from "react";
 
-const Accardion = () => {
-  return <div>Accardion</div>;
+interface AccordionProps {
+  title: string;
+  content: string;
+}
+
+const Accordion: React.FC<AccordionProps> = ({ title, content }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleAccordion = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="border-b border-gray-300">
+      <button
+        className="flex justify-between items-center w-full p-4 text-left"
+        onClick={toggleAccordion}
+      >
+        <span className="text-[16px] text-[#272727] w-[300px] font-[800] flex-grow">
+          {title}
+        </span>
+        <div
+          className={`flex items-center justify-center w-[24px] h-[24px] rounded-full transition-colors duration-300 ${
+            isOpen ? "bg-[#272727] text-white" : "bg-[#DFDFDF] text-[#272727]"
+          }`}
+        >
+          <svg
+            role="presentation"
+            focusable="false"
+            width="8"
+            height="6"
+            viewBox="0 0 8 6"
+            className={`transition-transform duration-300 ${
+              isOpen ? "rotate-180" : "rotate-0"
+            }`}
+          >
+            <path
+              d="m1 1.5 3 3 3-3"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="1.5"
+            ></path>
+          </svg>
+        </div>
+      </button>
+      <div
+        className={`overflow-hidden transition-all duration-300 ease-in-out ${
+          isOpen
+            ? "max-h-[500px] opacity-100 transform translate-y-0"
+            : "max-h-0 opacity-0 transform -translate-y-4"
+        }`}
+      >
+        <div className="px-4 py-[5px] text-gray-700">{content}</div>
+      </div>
+    </div>
+  );
 };
 
-export default Accardion;
+export default Accordion;
