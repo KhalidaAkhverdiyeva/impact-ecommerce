da5b1c8339a0963051725f6b08d212be338be34c
Khalida <xalide.haqverdiyeva@gmail.com>
2024-10-25 17:08:34 +0400

    feat:  quote section added

--- client-side/src/components/Designer Quote/quote.tsx
+++ client-side/src/components/Designer Quote/quote.tsx
@@ -4,8 +4,8 @@ import { GrFormNext, GrFormPrevious } from "react-icons/gr";
 
 const Quote = () => {
   const [currentQuoteIndex, setCurrentQuoteIndex] = useState(0);
+  const [isFadingOut, setIsFadingOut] = useState(false);
 
-  // Example quotes data (You can replace this with your actual quotes)
   const quotes = [
     {
       text: "We have always tried to focus on the people who were going to use the products, and we have always tried to design products for the life they live.",
@@ -14,44 +14,55 @@ const Quote = () => {
         "https://impact-theme-home.myshopify.com/cdn/shop/files/Mette.png?v=1656419248&width=140",
     },
     {
-      text: "Design is not just what it looks like and feels like. Design is how it works.",
+      text: "With our range of decorative objects, we want to create decoration that is not only pretty but that will give you a sense of pride and fullness.",
       author: "Steve Jobs",
-      image: "https://example.com/steve-jobs.png", // Replace with a valid image URL
+      image:
+        "https://impact-theme-home.myshopify.com/cdn/shop/files/Rolf.png?v=1656591294&width=140",
     },
-    // Add more quotes as needed
   ];
 
+  const changeQuote = (newIndex: React.SetStateAction<number>) => {
+    setIsFadingOut(true);
+    setTimeout(() => {
+      setCurrentQuoteIndex(newIndex);
+      setIsFadingOut(false);
+    }, 300);
+  };
+
   const nextQuote = () => {
-    setCurrentQuoteIndex((prevIndex) => (prevIndex + 1) % quotes.length);
+    changeQuote((currentQuoteIndex + 1) % quotes.length);
   };
 
   const prevQuote = () => {
-    setCurrentQuoteIndex(
-      (prevIndex) => (prevIndex - 1 + quotes.length) % quotes.length
-    );
+    changeQuote((currentQuoteIndex - 1 + quotes.length) % quotes.length);
   };
 
   return (
-    <section className="max-w-[1600px] mx-auto bg-[#363D88] py-[20px]">
-      <div className="text-white">
-        <div className="flex flex-col gap-[10px] max-w-[900px] mx-auto">
+    <section className="py-[20px]  md:mx-[32px] lg:mx-[48px] ">
+      <div className="text-white max-w-[1600px] mx-auto bg-[#363D88] py-[20px] md:py-[40px] lg:py-[60px]">
+        <div className="flex flex-col gap-[10px] max-w-[1410px] mx-auto">
           <div className="relative">
             <img
               src="data:image/svg+xml,%3Csvg width='86' height='55' viewBox='0 0 86 55' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M71.3163 54.6H42.5163L60.3163 0.400024H85.5163L71.3163 54.6ZM29.3163 54.6H0.716309L18.9163 0.400024H44.1163L29.3163 54.6Z' fill='rgba(233, 233, 233, 0.3)'/%3E%3C/svg%3E%0A"
               alt="quote"
-              className="absolute top-[0px] left-[10px] md:left-[-40px] w-[51px] h-[37px] md:w-[70px] md:h-[45px]"
+              className="absolute top-[0px] md:top-[15px] lg:left-[77px] left-[10px]  md:left-[50px] w-[51px] h-[37px] md:w-[70px] md:h-[45px]"
             />
-
-            <blockquote className="text-[26px] p-[20px] md:text-[36px] lg:text-[44px] relative font-[800] leading-[1.1] text-center">
+            <blockquote
+              className={`text-[26px] p-[20px] md:text-[36px] md:px-[70px] md:py-[40px] lg:text-[44px] relative font-[800] leading-[1.1] text-center transition-opacity duration-300 ease-in-out ${
+                isFadingOut ? "opacity-0" : "opacity-100"
+              }`}
+            >
               {quotes[currentQuoteIndex].text}
             </blockquote>
           </div>
           <div className="flex flex-col items-center gap-[10px] justify-center">
-            <div className="rounded-full w-[70px] h-[70px]">
+            <div className="rounded-full w-[70px] h-[70px] overflow-hidden transition-opacity duration-300">
               <img
                 src={quotes[currentQuoteIndex].image}
                 alt={quotes[currentQuoteIndex].author}
-                className="w-full h-full rounded-full"
+                className={`w-full h-full rounded-full transition-opacity duration-300 ease-in-out ${
+                  isFadingOut ? "opacity-0" : "opacity-100"
+                }`}
               />
             </div>
             <p className="text-white text-[14px] text-center md:text-[16px]">
@@ -64,7 +75,7 @@ const Quote = () => {
         <div className="flex justify-center items-center gap-[20px] mt-4 max-w-[900px] mx-auto">
           <button
             onClick={prevQuote}
-            className="  py-2 px-4 border-[1px] border-[#9CA3AF] w-[48px] h-[48px] rounded-full"
+            className="py-2 px-4 border-[1px] border-[#9CA3AF] w-[48px] h-[48px] rounded-full"
           >
             <GrFormPrevious />
           </button>
@@ -73,7 +84,7 @@ const Quote = () => {
             {quotes.map((_, index) => (
               <button
                 key={index}
-                onClick={() => setCurrentQuoteIndex(index)}
+                onClick={() => changeQuote(index)}
                 className={`h-2 w-2 rounded-full mx-1 transition-all duration-300 ${
                   index === currentQuoteIndex ? "bg-white" : "bg-gray-400"
                 }`}
@@ -82,7 +93,7 @@ const Quote = () => {
           </div>
           <button
             onClick={nextQuote}
-            className="  py-2 px-4 border-[1px] border-[#9CA3AF] w-[48px] h-[48px] rounded-full"
+            className="py-2 px-4 border-[1px] border-[#9CA3AF] w-[48px] h-[48px] rounded-full"
           >
             <GrFormNext />
           </button>
