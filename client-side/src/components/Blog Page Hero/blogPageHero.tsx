a74b14bafbadb03f3b526d9c4d5936a86b95cb28
Khalida <xalide.haqverdiyeva@gmail.com>
2024-11-01 23:29:55 +0400

    feat: section chnage buttons added

--- client-side/src/components/Blog Page Hero/blogPageHero.tsx
+++ client-side/src/components/Blog Page Hero/blogPageHero.tsx
@@ -1,11 +1,23 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { Header } from "../Layout/Header/header";
 
 const BlogPageHero = () => {
+  const [activeSection, setActiveSection] = useState("All posts");
+
+  const sections = [
+    "All posts",
+    "Collection",
+    "Inspiration",
+    "News",
+    "Recipe",
+    "Story",
+  ];
+
   return (
     <div className="relative pb-[80px]">
       <Header />
-      <div className="bg-[#3C619E] w-[100%] h-[570px] text-[white] flex items-center justify-center flex-col text-center">
+      <div className="bg-[#3C619E] w-[100%] h-[570px] text-[white] flex items-center justify-center flex-col text-center relative">
         <h1 className="text-[50px] md:text-[60px] leading-[1] pb-[10px] lg:text-[80px] font-[800] max-w-[380px] md:max-w-[700px] lg:max-w-[1000px]">
           Stories to inspire you
         </h1>
@@ -19,32 +31,50 @@ const BlogPageHero = () => {
             placeholder="Email"
             className="border border-[#476AA4] bg-transparent w-[100%] placeholder:text-[#9CAECE] md:flex-[60%] text-white pr-[32px] pl-[20px] py-[16px] outline-none"
           />
-          <button className="bg-white font-[800] text-[#3C619E] w-[100%] md:flex-[40%] flex items-center justify-center gap-[10px]  text-center px-[32px] py-[16px]">
+          <button className="bg-white font-[800] text-[#3C619E] w-[100%] md:flex-[40%] flex items-center justify-center gap-[10px] text-center px-[32px] py-[16px]">
             <svg
               role="presentation"
               fill="none"
               focusable="false"
-              stroke-width="1.5"
+              strokeWidth="1.5"
               width="18"
               height="18"
               viewBox="0 0 18 14"
             >
               <path
-                clip-rule="evenodd"
+                clipRule="evenodd"
                 d="M1 2.5A1.5 1.5 0 0 1 2.5 1h13A1.5 1.5 0 0 1 17 2.5v9a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 1 11.5v-9Z"
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               ></path>
               <path
                 d="m16 2-5.61 4.506c-.82.659-1.96.659-2.78 0L2 2"
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               ></path>
             </svg>
             Submit
           </button>
+        </div>
+
+        <div className="absolute bottom-0 w-full">
+          <div className="flex gap-[15px] md:justify-center overflow-x-auto px-[20px]">
+            {sections.map((section) => (
+              <button
+                key={section}
+                onClick={() => setActiveSection(section)}
+                className={`px-[20px] lg:px-[24px] lg:py-[14px] py-[10px] border font-[700] text-[16px] md:text-[18px] whitespace-nowrap border-transparent ${
+                  activeSection === section
+                    ? "bg-white text-black transition-all duration-300 ease-in-out"
+                    : "text-[#9EB0CF] hover:text-[white] transition-all duration-300 ease-in-out"
+                }`}
+              >
+                {section}
+              </button>
+            ))}
+          </div>
         </div>
       </div>
     </div>
