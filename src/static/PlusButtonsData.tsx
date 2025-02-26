f1e0ac3cc575035453999a80a63a05bb5172b4bd
Khalida <xalide.haqverdiyeva@gmail.com>
2025-02-26 16:53:33 +0400

    fix: image chnage fix

--- src/static/PlusButtonsData.tsx
+++ src/static/PlusButtonsData.tsx
@@ -1,14 +1,14 @@
 export const sampleModalInfo = [
   {
     iconImg:
-      "https://impact-rho.vercel.app/_next/image?url=https%3A%2F%2Fimpact-theme-home.myshopify.com%2Fcdn%2Fshop%2Ffiles%2Ficon-brightness.png%3Fv%3D1653312415%26width%3D48&w=48&q=75",
+      "https://impact-theme-home.myshopify.com/cdn/shop/files/icon-brightness.png?v=1653312415&width=48",
     altImg: "sunIicon",
     title: "Dimmable",
     info: "Integrated touch step dimmer switch with three standard settings.",
   },
   {
     iconImg:
-      "https://impact-rho.vercel.app/_next/image?url=https%3A%2F%2Fimpact-theme-home.myshopify.com%2Fcdn%2Fshop%2Ffiles%2Ficon-lightbulb.png%3Fv%3D1653312427%26width%3D48&w=48&q=75",
+      "https://impact-theme-home.myshopify.com/cdn/shop/files/icon-lightbulb.png?v=1653312427&width=48",
     altImg: "bulbIcon",
     title: "Light source",
     info: "Includes replaceable LED light source ensuring a life span of at least 25,000 hours of use",
@@ -18,14 +18,14 @@ export const sampleModalInfo = [
 export const toasterModalInfo = [
   {
     iconImg:
-      "https://impact-rho.vercel.app/_next/image?url=https%3A%2F%2Fimpact-theme-home.myshopify.com%2Fcdn%2Fshop%2Ffiles%2Ficon-brightness.png%3Fv%3D1653312415%26width%3D48&w=48&q=75",
+      "https://impact-theme-home.myshopify.com/cdn/shop/files/icon-brightness.png?v=1653312415&width=48",
     altImg: "sunIicon",
     title: "Warming tray",
     info: "Top doubles as a warming tray; rest your toast, croissant, or baguette on top to warm it.",
   },
   {
     iconImg:
-      "https://impact-rho.vercel.app/_next/image?url=https%3A%2F%2Fimpact-theme-home.myshopify.com%2Fcdn%2Fshop%2Ffiles%2Ficon-lightbulb.png%3Fv%3D1653312427%26width%3D48&w=48&q=75",
+      "https://impact-theme-home.myshopify.com/cdn/shop/files/icon-lightbulb.png?v=1653312427&width=48",
     altImg: "bulbIcon",
     title: "Toasting level",
     info: "Includes a practical browning dial to set your ideal toasting level.",
